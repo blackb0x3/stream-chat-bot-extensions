f23cb5dd50358aacebacd6e0dda407b987d5387d
Black Box <developer.luke.barker3@outlook.com>
2022-04-15 02:03:22 +0100

    Remove example code and log request data

--- RunModule/__init__.py
+++ RunModule/__init__.py
@@ -1,24 +1,21 @@
+import azure.functions as func
 import logging
 
-import azure.functions as func
+from shared.enums import HttpStatusCode
+
+ARG_DELIMITER = '|'
 
 
 def main(req: func.HttpRequest) -> func.HttpResponse:
     logging.info('Python HTTP trigger function processed a request.')
 
-    name = req.params.get('name')
-    if not name:
-        try:
-            req_body = req.get_json()
-        except ValueError:
-            pass
-        else:
-            name = req_body.get('name')
+    module_name = req.route_params.get('moduleName')
+    command_name = req.route_params.get('commandName')
+    args = req.route_params.get('args').split(ARG_DELIMITER)
 
-    if name:
-        return func.HttpResponse(f"Hello, {name}. This HTTP triggered function executed successfully.")
-    else:
-        return func.HttpResponse(
-             "This HTTP triggered function executed successfully. Pass a name in the query string or in the request body for a personalized response.",
-             status_code=200
-        )
+    logging.debug({ module_name, command_name, args })
+
+    return func.HttpResponse(
+        "This HTTP triggered function executed successfully.",
+        status_code=HttpStatusCode.OK.value
+    )
